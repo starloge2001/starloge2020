e564a5295c7f324a6d378dc2b17a33c7b36e6597
starloge2001 <starloge@naver.com>
2020-12-04 16:41:54 +0900

    수정

--- ldjex/src/ldjex/Sososoo.java
+++ ldjex/src/ldjex/Sososoo.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Sososoo {
 
 	public static void main(String[] args) {
-		System.out.println("첫번째 숫자를 입력하시오 : ");
+		System.out.println("첫번째 세자리 숫자를 입력하시오 : ");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		System.out.println("두번째 숫자를 입력하시오 : ");
+		System.out.println("두번째 세자리 숫자를 입력하시오 : ");
 		int b = sc.nextInt();
 		int c;
 		int d;
@@ -17,10 +17,10 @@ public class Sososoo {
 		e = b % 100;
 		d = e / 10;
 		e = e % 10;
-		System.out.println(e * a);
-		System.out.println(d * a);
-		System.out.println(c * a);
-		System.out.println(b * a);
+		System.out.println(e * a + "(3)");
+		System.out.println(d * a + "(4)");
+		System.out.println(c * a + "(5)");
+		System.out.println(b * a + "(6)");
 		
 	}
 
