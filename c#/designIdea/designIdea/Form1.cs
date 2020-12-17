12f7c7e986e20fba7ae846e03632bc787ae1bb2c
starloge2001 <starloge@naver.com>
2020-12-17 19:18:14 +0900

    1

--- c#/designIdea/designIdea/Form1.cs
+++ c#/designIdea/designIdea/Form1.cs
@@ -15,6 +15,37 @@ namespace designIdea
         public Form1()
         {
             InitializeComponent();
+            panelLeft.Height = buttonDashboard.Height;
+            panelLeft.Top = buttonDashboard.Top;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void buttonDashboard_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonDashboard.Height;
+            panelLeft.Top = buttonDashboard.Top;
+        }
+
+        private void buttonSellers_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonSellers.Height;
+            panelLeft.Top = buttonSellers.Top;
+        }
+
+        private void buttonCalender_Click(object sender, EventArgs e)
+        {
+            panelLeft.Top = buttonCalender.Top;
+            panelLeft.Height = buttonCalender.Height;
+        }
+
+        private void buttonTasks_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonTasks.Height;
+            panelLeft.Top = buttonTasks.Top;
         }
     }
 }
