2ef4d09b7ba54c318b7e5bfba8ee0e57e6cdc736
starloge2001 <starloge@naver.com>
2021-01-11 16:47:59 +0900

    20210111

---
+++ Backjoon/src/Ex034.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ex034 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int k = s.nextInt();
+		String[] a = new String[k];
+		int sum=0;
+		int con=0;
+		for (int i = 0; i < k; i++) {
+			a[i]=s.next();
+			int[] count = new int[a[i].length()];
+			for (int j = 0; j < count.length; j++) {
+				if(a[j].equals("o")) {
+					con++;
+				}
+				else {
+					con=0;
+				}
+				sum=sum+con;
+				System.out.println(sum);
+			}
+			System.out.println(sum);
+		}				
+		
+	}
+
+}
