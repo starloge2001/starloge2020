924ad5aaac8876c24fc383680b2581228e7b99dc
starloge2001 <starloge@naver.com>
2021-01-25 16:44:21 +0900

    20210125

---
+++ JAVA/Backjoon/src/Ex042.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex042 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.println("*");
+		}
+	}
+
+}
