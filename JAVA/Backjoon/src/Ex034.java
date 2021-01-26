68a288cc732b145705e968f2ef85c63f60d29603
starloge2001 <starloge@naver.com>
2021-01-26 16:33:55 +0900

    20210126

--- JAVA/Backjoon/src/Ex034.java
+++ JAVA/Backjoon/src/Ex034.java
@@ -5,24 +5,23 @@ public class Ex034 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int k = s.nextInt();
-		String[] a = new String[k];
-		int sum=0;
-		int con=0;
+		String a[]=new String[k];
 		for (int i = 0; i < k; i++) {
 			a[i]=s.next();
-			int[] count = new int[a[i].length()];
-			for (int j = 0; j < count.length; j++) {
-				if(a[j].equals("o")) {
+			int sum=0;
+			int con=0;
+			for (int j = 0; j < a[i].length(); j++) {
+				if(a[i].charAt(j)=='O') {
 					con++;
 				}
 				else {
 					con=0;
 				}
 				sum=sum+con;
-				System.out.println(sum);
 			}
 			System.out.println(sum);
-		}				
+			
+		}		
 		s.close();
 	}
 
--- JAVA/Backjoon/src/Ex042.java
+++ JAVA/Backjoon/src/Ex042.java
@@ -6,8 +6,12 @@ public class Ex042 {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 		for (int i = 0; i < n; i++) {
-			System.out.println("*");
+			for (int j = 0; j < n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
+		s.close();
 	}
 
 }
