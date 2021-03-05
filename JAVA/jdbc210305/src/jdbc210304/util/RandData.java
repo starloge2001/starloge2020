08522695466d1b69ef312f465d7e2ce5e7c832b5
starloge2001 <starloge@naver.com>
2021-03-05 16:30:25 +0900

    0305

--- JAVA/jdbc210305/src/jdbc210304/util/RandData.java
+++ JAVA/jdbc210305/src/jdbc210304/util/RandData.java
@@ -1,5 +1,13 @@
 package jdbc210304.util;
 
 public class RandData {
-
+	static String name[] ={"홍길동", "김길동", "이길동", "박길동", "최길동"};
+	static int age[] = {10,15,20,25,30};
+	static String[] tel = {"010-1111-1111",
+        "010-2222-2222", "010-2222-3333",
+        "010-2222-4444", "010-2222-5555"};
+	static String[] email = {"hong@naver.com",
+        "kim@naver.com", "lee@naver.com",
+        "park@naver.com", "choi@naver.com"};
+	
 }
